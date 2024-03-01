74f5b1a44dffea14215a59476e4c5893040d50cf
Matt <38063055+Terracom12@users.noreply.github.com>
2024-02-29 19:58:33 -0800

    Initial Commit.

---
+++ basic_example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// Returns factorial of n (n!)
+// Examples:
+//  factorial(1) = 1 = 1
+//  factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+int factorial(unsigned int n)
+{
+    if (n <= 1) return 1;
+
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int* intPtr = new int{};
+    std::cout << "intPtr = " << intPtr <<  " | *intPtr = " << *intPtr << '\n';
+    delete intPtr;
+
+    const int numIterations = 3;
+    for (int i = 0; i < numIterations; ++i) {
+        std::cout << "i = " << i << '\n';
+    }
+
+    std::cout << "8! = " << factorial(8) << std::endl;
+
+    return 0;
+}
