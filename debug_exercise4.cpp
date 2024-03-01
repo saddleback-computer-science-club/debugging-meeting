be8cad8fcb08ec80cef6a9f49f943d015015ba04
Matt <38063055+Terracom12@users.noreply.github.com>
2024-03-01 14:57:16 -0800

    Added exercise 4.

---
+++ debug_exercise4.cpp
@@ -0,0 +1,32 @@
+// Saddleback Computer Science S.A.M.E. Cohort
+// March 1st, 2024
+
+/*
+DEBUG EXERCISE 4:
+
+Congratulations making it here!
+
+This exercise is meant to be DIY where you can play around with all of the
+tools we went over including:
+- The built-in debugger
+- clang-tidy and clang-format
+- valgrind
+- Tests
+
+Here's a few ideas for experimentation:
+- Dynamically allocate an array in memory and see how valgrind reactsd
+- Write some terribly formatted code, maybe everything on the same line,
+and see how clang-format changes it
+- Write code that attempts to access an element outside the bounds of a 
+dynamically allocated array.
+- Create some basic algorithms for sorting an array/vector and create unit
+tests for them. Try the same with any other algorithms you know!
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from exercise 4!" << std::endl;
+    // Insert code here...
+}
