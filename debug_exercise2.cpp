22e4340642fdad171063b60135b5c4aef232d253
Matt <38063055+Terracom12@users.noreply.github.com>
2024-03-01 06:04:26 -0800

    Fixed everything.

--- debug_exercise2.cpp
+++ debug_exercise2.cpp
@@ -5,49 +5,111 @@
 
 Great job on completing exercise 1!
 
-This exercise introduces a couple new concepts, namely std::vector and some
-very basic recursion. If you're not sure what either of these are, don't worry.
-The examples used here aren't too hard to wrap your head around.
+This exercise introduces a couple new concepts, namely linked lists, std::vector
+and some very basic recursion. If you're not sure what either of these are, 
+don't worry. The examples used here shouldn't be too hard to understand.
 
 Basically: 
   - a std::vector functions as an array, but its size can grow/shrink during
   runtime. The `push_back` function
-  - recursion is when a function calls itself 
+  See: https://en.cppreference.com/w/cpp/container/vector
+  - recursion is when a function calls itself
 
 Remember the "CALL STACK" menu, which will help with analyzing the behaviour of
 recursive functions!
 
-There are a total of XXXXX ERRORS.
+The amount of comments has lessened to encourage you to figure out the behaviour
+of functions yourself.
+
+There are a total of XXXXX LOGICAL ERRORS in this program.
+
+If you can figure out what these algorithms should look like, you should have a
+very easy time finding the errors.
 */
 
 #include <iostream>
 #include <vector>
-using std::cout, std::vector;
+using namespace std;
 
-// Computes the fibin
+struct LinkedList
+{
+    int data;
+
+    LinkedList* next;
+};
+
+// Prints the elements of vec to standard out
+void print_vec(const vector<int>& vec);
+
+// Reverses the elements of a vector in-place
+void reverse_vec(vector<int>& vec);
+
+// Prints the nodes of a linked list, from back to front
+void print_list(LinkedList* front);
+
+// Creates a linked list from the elements in a vector
+// Returns a pointer to the front node of the list.
+LinkedList* construct_list(const vector<int>& from);
 
 int main()
 {
+    vector vec = { 1, 2, 3, 4, 5 };
 
+    reverse_vec(vec);
+
+    print_vec(vec);
+
+    LinkedList* front = construct_list({ 42, 30, 10, 12 });
+
+    print_list(front);
 }
 
-int binary_search(int array[], int value, int size)
+void print_vec(const vector<int>& vec)
 {
-    // TODO: Break this lol
-    int low = 0, high = size - 1;
-    int mid{};
+    cout << "[ ";
+    // Range-based for loop. Prevents off by one errors!
+    // See: https://www.learncpp.com/cpp-tutorial/range-based-for-loops-for-each
+    for (int n : vec) {
+        cout << n << ' ';
+    }
+    cout << "]\n";
+}
 
-    while (low <= high) {
-        mid = (low + high) / 2;
-
-        if (array[mid] == value) {
-
-        } else if (array[mid] > value) {
-            high = mid - 1;
-        } else {
-            high = mid + 1;
-        }
+void reverse_vec(vector<int> &vec)
+{
+    if (vec.size() == 0) {
+        return;
     }
 
-    return -1;
+    int num = vec.back();
+    vec.pop_back();
+
+    reverse_vec(vec);
+
+    vec.insert(vec.begin(), num);
+}
+
+void print_list(LinkedList* front)
+{
+    if (front != nullptr) {
+        print_list(front->next);
+
+        cout << front->data << ' ';
+    }
+}
+
+
+LinkedList* construct_list(const vector<int>& from)
+{
+    static int i = from.size();
+    LinkedList* front = nullptr;
+
+    if (!i--) {
+        return nullptr;
+    }
+
+    front = new LinkedList{from[i]};
+    front->next = construct_list(from);
+
+    return front;
 }
