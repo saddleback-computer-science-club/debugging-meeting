c7d0a88542cd520eedd7584b85421f620639bd05
Matthew Reese <awesomepop012@hotmail.com>
2024-03-01 04:17:59 -0800

    Begin exercise 2.

--- debug_exercise2.cpp
+++ debug_exercise2.cpp
@@ -1,15 +1,30 @@
 // Saddleback Computer Science S.A.M.E. Cohort
 // March 1st, 2024
 
-/* DESCRIPTION
+/* DEBUG EXERCISE 2:
+
+Great job on completing exercise 1!
+
+This exercise introduces a couple new concepts, namely std::vector and some
+very basic recursion. If you're not sure what either of these are, don't worry.
+The examples used here aren't too hard to wrap your head around.
+
+Basically: 
+  - a std::vector functions as an array, but its size can grow/shrink during
+  runtime. The `push_back` function
+  - recursion is when a function calls itself 
+
+Remember the "CALL STACK" menu, which will help with analyzing the behaviour of
+recursive functions!
+
+There are a total of XXXXX ERRORS.
 */
 
 #include <iostream>
+#include <vector>
+using std::cout, std::vector;
 
-// Returns the index of a value in an array using the binary search algorithm
-// or -1 if value does not exist in array.
-// see: https://www.programiz.com/dsa/binary-search if review is needed
-int binary_search(int array[], int value);
+// Computes the fibin
 
 int main()
 {
